95f5d9fc1f8443c8eccb9c24b3dd2b6f33a9b3ee
Lukas Mohs <ga47boq@mytum.de>
2017-12-07 15:44:27 +0100

    plottet PA on EI

--- script.R
+++ script.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(car)
 
 setwd("/Users/lukasmohs/Desktop/Innovation-Analysis/")
 data = read.csv("cleaned-responses.csv",sep=';')
@@ -6,8 +7,13 @@ summary(data)
 data <- mutate(data, PA = (X11a+X11b+X11c+X11d+X11e)/5)
 data <- mutate(data, SN = (X13a+X13b+X13c)/3)
 data <- mutate(data, PBC = (X15a + X15b + X15c + X15d + X15e + X15f)/6)
-data <- mutate(data, IE = (X18a + X18b + X18c + X18d + X18e + X18f)/6)
+data <- mutate(data, EI = (X18a + X18b + X18c + X18d + X18e + X18f)/6)
 data <- mutate(data, SEBA = ifelse(Attended.SEBA == 'yes', 1, 0))
+data <- mutate(data, Entrepreneur = ifelse(Already.Entrepreneur == 'yes', 1, 0))
+
+#Modeling effect of IE on Entrepreneur
+fitIEonEntrpreneur <- lm(Entrepreneur ~ EI, data=data)
+summary(fitIEonEntrpreneur)
 
 #Modeling effect of SEBA on PA
 fitPA <- lm(PA ~ SEBA, data=data)
@@ -19,9 +25,15 @@ summary(fitSN)
 fitPBC <- lm(PBC ~ SEBA, data=data)
 summary(fitPBC)
 #Modeling effect of SEBA on IE
-fitSEBAonIE <- lm(IE ~ SEBA, data=data)
-summary(fitSEBAonIE)
+fitSEBAonEI <- lm(EI ~ SEBA, data=data)
+summary(fitSEBAonEI)
 
 #Modeling effect of Ajzens antecedents on IE
-fitAjzen <- lm(IE ~ PA + SN + PBC, data=data)
+fitAjzen <- lm(EI ~ PA + SN + PBC, data=data)
 summary(fitAjzen)
+#plot(fitAjzen)
+
+plot(data$PA,data$EI,  main="EI vs. PA", 
+     xlab="PA ", ylab="EI", pch=19)
+
+abline(lm(EI ~ PA, data = data), col="red") # regression line (y~x) 
